d83c3846c6d4606b3f51061a94b0322358cb4afd
Liam Bindle <liambindle@gmail.com>
2020-08-04 18:08:02 -0600

    foo

---
+++ Sleep/sleep/sleep.ino
@@ -0,0 +1,34 @@
+#include <avr/sleep.h>
+#define interruptPin 2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void wakeUp() {
+  Serial.println("Interrupt Fired!");
+  sleep_disable();
+  detachInterrupt(0);
+}
+
+void go_to_sleep() {
+  sleep_enable();
+  attachInterrupt(0, wakeUp, LOW);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  sleep_cpu();
+  Serial.println("Just woke up!");
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5000);
+  Serial.println("Going to sleep.");
+  go_to_sleep();
+}
